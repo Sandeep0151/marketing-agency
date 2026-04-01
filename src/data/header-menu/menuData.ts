5da62a478e1d04ae8005233acf17ea1fa3538dce
Sandeep0151 <sandeepyaduvanshi28@gmail.com>
2026-04-02 02:38:37 +0530

    initial commit

---
+++ src/data/header-menu/menuData.ts
@@ -0,0 +1,47 @@
+import { MenuItem } from "@/types/menu-d-t";
+
+const headerMenuData: MenuItem[] = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#home",
+    active: true,
+  },
+  {
+    id: 2,
+    title: "About",
+    link: "#about",
+  },
+  {
+    id: 3,
+    title: "Services",
+    link: "#services",
+  },
+  {
+    id: 4,
+    title: "Portfolio",
+    link: "#projects",
+  },
+  {
+    id: 5,
+    title: "Pricing",
+    link: "#pricing",
+  },
+  {
+    id: 6,
+    title: "Testimonials",
+    link: "#testimonials",
+  },
+  {
+    id: 7,
+    title: "Blog",
+    link: "#blog",
+  },
+  {
+    id: 8,
+    title: "Contact",
+    link: "#contact",
+  },
+];
+
+export default headerMenuData;
